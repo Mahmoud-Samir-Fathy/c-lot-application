732185349a6fcd42e5326045e314497b48ccc91b
Mahmoud-Samir-Fathy <mahmoudsamir0777@gmail.com>
2024-12-27 14:59:54 +0200

    Working on cart pages

--- lib/features/cart/data/data_sources/fire_base_get_cart_items_data_source.dart
+++ lib/features/cart/data/data_sources/fire_base_get_cart_items_data_source.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
-import 'package:e_commerce_app/features/cart/domain/entities/get_from_cart_entity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class FireBaseGetCartItemsDataSource {
@@ -18,9 +17,7 @@ class FireBaseGetCartItemsDataSourceImpl
           .doc(user!.uid)
           .collection('cart')
           .get();
-      final cartItems = cartSnapshot.docs
-          .map((doc) => GetFromCartEntity.fromJson(doc.data()))
-          .toList();
+      final cartItems = cartSnapshot.docs.map((doc) => doc.data()).toList();
       return Right(cartItems);
     } on FirebaseException catch (e) {
       return Left(e.message);
--- lib/features/cart/presentation/pages/cart_page.dart
+++ lib/features/cart/presentation/pages/cart_page.dart
@@ -1,7 +1,12 @@
-import 'package:e_commerce_app/core/asset_manager/app_images.dart';
 import 'package:e_commerce_app/core/utilis/app_colors.dart';
+import 'package:e_commerce_app/dependency_injection.dart';
+import 'package:e_commerce_app/features/cart/domain/entities/get_from_cart_entity.dart';
+import 'package:e_commerce_app/features/cart/presentation/manager/cubit.dart';
+import 'package:e_commerce_app/features/cart/presentation/manager/states.dart';
 import 'package:e_commerce_app/shared_widgets/app_bar/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CartPage extends StatelessWidget {
 
@@ -14,119 +19,131 @@ class CartPage extends StatelessWidget {
         hideBack: false,
         title: Text('Cart'),
       ),
-      body:ListView.separated(
-              itemBuilder: (context, index) => buildCartItem(),
-              separatorBuilder: (context, index) => const SizedBox(height: 10),
-              itemCount: 10,
-            ),
+      body: BlocProvider(
+        create: (context) => sl<GetFromCartCubit>()..getItemsFromCart(),
+        child: BlocConsumer<GetFromCartCubit, GetFromCartStates>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            if (state is GetFromCartSuccessState) {
+              List<GetFromCartEntity> cartItem = state.cart;
+              return ListView.separated(
+                itemBuilder: (context, index) => buildCartItem(cartItem[index]),
+                separatorBuilder: (context, index) =>
+                const SizedBox(height: 10),
+                itemCount: state.cart.length,
+              );
+            }
+            return Container();
+          },
+        ),
+      ),
     );
   }
-  Widget buildCartItem()=>Container(
-    width: double.infinity,
-    height: 100,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(20),
-      color: AppColors.secondBackground,
-    ),
-    child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: const Image(
-              height: 80,
-              width: 80,
-              image: AssetImage(AppImages.defaultImage),
-              fit: BoxFit.cover,
-            ),
-          ),
-          const SizedBox(width: 15),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Row(
+
+  Widget buildCartItem(GetFromCartEntity cart) =>
+      Container(
+        width: double.infinity,
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: AppColors.secondBackground,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image(
+                  height: 80,
+                  width: 80,
+                  image: NetworkImage(cart.productImage),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(width: 15),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: Text(
-                        'Ay 5ra',
-                        style: TextStyle(
-                          color: AppColors.buttonTextColor,
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            cart.productTitle,
+                            style: const TextStyle(
+                              color: AppColors.buttonTextColor,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
+                        const Spacer(),
+                        Text(
+                          "\$${cart.productPrice}",
+                          style: const TextStyle(
+                            color: AppColors.buttonTextColor,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
-                    Spacer(),
-                    Text(
-                      "\$${10}",
-                      style: TextStyle(
-                        color: AppColors.buttonTextColor,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  children: [
-                    const Text(
-                      'Size - ',
-                      style: TextStyle(
-                        color: AppColors.searchColor,
-                        fontSize: 15,
-                      ),
-                    ),
-                    const Text(
-                      "${20}",
-                      style: TextStyle(
-                        color: AppColors.buttonTextColor,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(width: 15),
-                    const Text(
-                      'Color - ',
-                      style: TextStyle(
-                        color: AppColors.searchColor,
-                        fontSize: 15,
-                      ),
-                    ),
-                    const Text(
-                     '5rawy',
-                      style: TextStyle(
-                        color: AppColors.buttonTextColor,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Spacer(),
-                    FloatingActionButton(
-                      onPressed: () {
-                        // Add logic to remove item from cart or decrease quantity
-                      },
-                      mini: true,
-                      backgroundColor: AppColors.primary,
-                      child: const Icon(
-                        Icons.remove,
-                        color: AppColors.buttonTextColor,
-                      ),
+                    const SizedBox(height: 8),
+                    Row(
+                      children: [
+                        const Text(
+                          'Size - ',
+                          style: TextStyle(
+                            color: AppColors.searchColor,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(
+                          "${cart.productSize}",
+                          style: const TextStyle(
+                            color: AppColors.buttonTextColor,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 15),
+                        const Text(
+                          'Color - ',
+                          style: TextStyle(
+                            color: AppColors.searchColor,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(
+                          cart.productColor,
+                          style: const TextStyle(
+                            color: AppColors.buttonTextColor,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Spacer(),
+
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.r),
+                            color: AppColors.primary
+                          ),
+                          child: Icon(Icons.remove,size: 30.sp,),
+                        )
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
-    ),
-  );
+        ),
+      );
 }
 
 
