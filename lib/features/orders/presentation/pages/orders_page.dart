0a843c0b54a82a6157ae9a6a7a22711097511d23
Mahmoud-Samir-Fathy <mahmoudsamir0777@gmail.com>
2025-01-14 18:54:09 +0200

    notification & Orders

--- lib/features/orders/presentation/pages/orders_page.dart
+++ lib/features/orders/presentation/pages/orders_page.dart
@@ -1,13 +1,22 @@
-import 'package:e_commerce_app/features/orders/presentation/widgets/empty_orders.dart';
+import 'package:e_commerce_app/features/orders/presentation/widgets/order_widget.dart';
+import 'package:e_commerce_app/shared_widgets/app_bar/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: EmptyOrders(),
+    return Scaffold(
+      appBar: const BasicAppbar(
+        hideBack: true,
+        title: Text('Orders'),
+      ),
+      body: ListView.separated(
+          itemBuilder: (context, index) => const OrderWidget(),
+          separatorBuilder: (context, index) => SizedBox(height: 5.h,),
+          itemCount: 5),
     );
   }
 }
--- lib/features/orders/presentation/widgets/order_widget.dart
+++ lib/features/orders/presentation/widgets/order_widget.dart
@@ -1,10 +1,56 @@
+import 'package:e_commerce_app/core/utilis/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OrderWidget extends StatelessWidget {
   const OrderWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 15.0.w),
+      child: Container(
+        height: 90.h,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.r),
+            color: AppColors.secondBackground),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+          child: Row(
+            children: [
+              Icon(
+                Icons.archive,
+                size: 45.sp,
+              ),
+              SizedBox(
+                width: 10.w,
+              ),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Order  #####################',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    const Text('No of items:4'),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              IconButton(
+
+                  onPressed: () {}, icon: const Icon(Icons.arrow_forward_ios_outlined))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
