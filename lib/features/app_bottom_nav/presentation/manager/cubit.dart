3fea00348162be75fb4f6e6d9c67adff1db784a0
Mahmoud-Samir-Fathy <mahmoudsamir0777@gmail.com>
2025-01-24 01:22:50 +0200

    working on orders

--- lib/features/app_bottom_nav/presentation/manager/cubit.dart
+++ lib/features/app_bottom_nav/presentation/manager/cubit.dart
@@ -1,7 +1,7 @@
 import 'package:e_commerce_app/features/app_bottom_nav/presentation/manager/states.dart';
 import 'package:e_commerce_app/features/home/presentation/pages/home_page.dart';
 import 'package:e_commerce_app/features/notifications/presentation/pages/notification_page.dart';
-import 'package:e_commerce_app/features/orders/presentation/widgets/order_by_status.dart';
+import 'package:e_commerce_app/features/orders/presentation/widgets/order_screen.dart';
 import 'package:e_commerce_app/features/settings/presentation/pages/settings_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
